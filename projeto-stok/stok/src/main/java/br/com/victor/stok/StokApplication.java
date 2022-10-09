bb78c73c86dfcf00ac1f7ae771152d9050546a2f
Victor Araujo Barbosa <victorabarbosa03@gmail.com>
2022-10-09 16:09:05 -0300

    incluindo a classe StokApplication que inicia/roda a API

---
+++ projeto-stok/stok/src/main/java/br/com/victor/stok/StokApplication.java
@@ -0,0 +1,14 @@
+package br.com.victor.stok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication	
+public class StokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StokApplication.class, args);
+	}
+
+}
