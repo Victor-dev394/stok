1c53513ccee7ba80ddb95c285039434a919543fe
Victor Araujo Barbosa <victorabarbosa03@gmail.com>
2022-10-09 16:10:40 -0300

    incluindo a classe StokApplicationTests

---
+++ projeto-stok/stok/src/test/java/br/com/victor/stok/StokApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victor.stok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
