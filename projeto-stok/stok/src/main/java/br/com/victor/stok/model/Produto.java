7c63a20d96e353b80e539c572f40c1e8c2b70ba0
Victor Araujo Barbosa <victorabarbosa03@gmail.com>
2022-10-09 16:14:22 -0300

    incluindo a classe modelo de Produto que representa a entidade produto do banco de dados

---
+++ projeto-stok/stok/src/main/java/br/com/victor/stok/model/Produto.java
@@ -0,0 +1,75 @@
+package br.com.victor.stok.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String descricao;
+	private LocalDate dataCadastro = LocalDate.now();
+	private String fornecedor;
+	@Enumerated(EnumType.STRING)
+	private Categoria categoria;
+	
+	public Produto() {
+		
+	}
+	
+	public Produto(String nome, String descricao, String fornecedor, Categoria categoria) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.fornecedor = fornecedor;
+		this.categoria = categoria;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	public String getFornecedor() {
+		return fornecedor;
+	}
+	public void setFornecedor(String fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+}
